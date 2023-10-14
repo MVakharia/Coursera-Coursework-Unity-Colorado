50dd8eedce7dec048edfa1a2552569da1a708317
MVakharia <mvakharia.bcm@gmail.com>
2023-10-14 14:10:16 +0100

    Uploaded Course 1, Exercise 1.

---
+++ Introduction to C# Programming and Unity/Exercise 1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name");
+
+            Console.WriteLine("My best friend's name");
+        }
+    }
+}
