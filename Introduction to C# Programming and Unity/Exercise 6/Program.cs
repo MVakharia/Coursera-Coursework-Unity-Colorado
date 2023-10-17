60f40fa820d1af09fe7cde052e4b720a0f526feb
MVakharia <mvakharia.bcm@gmail.com>
2023-10-17 15:02:55 +0100

    Create Program.cs

---
+++ Introduction to C# Programming and Unity/Exercise 6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an angle in degrees.");
+
+            float angleInDegrees = float.Parse(Console.ReadLine());
+
+            float angleInRadians = angleInDegrees * ((float)Math.PI / 180);
+
+            Console.WriteLine("The sine of the angle is " + Math.Sin((double)angleInRadians));
+
+            Console.WriteLine("The cosine of the angle is " + Math.Cos((double)angleInRadians));
+        }
+    }
+}
